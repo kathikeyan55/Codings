ad6fd69ff0bd976086b434be9695a6da7c5897d4
karthikeyan <89398512+kathikeyan55@users.noreply.github.com>
2021-10-19 14:08:50 +0530

    i done that

--- java/qucicsort.java
+++ java/qucicsort.java
@@ -4,37 +4,36 @@ import java.util.Arrays;
 public class qucicsort {
     public static void main(String[] args) {
         int arr[]={1,2,6,4,3,0};
-        quicksort(arr,0,arr.length-1);
+        quickSort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
     }
 
-    private static void quicksort(int[] arr, int start, int end) {
-        if(start>end){return;}
-            int pi=partiton(arr,start,end);
-            quicksort(arr,start,pi-1);
-            quicksort(arr,pi+1  ,end);
-
+    static void quickSort(int arr[], int low, int high)
+    {
+        if(low >high) return ;
+        int p=partition(arr, low, high);
+         quickSort(arr,low ,p-1);
+         quickSort(arr,p+1,high);
+        
     }
-       /*
-       5 4 3 2 1
-        */
-    private static int partiton(int[] arr, int start, int end) {
-        int povi=arr[end]; //5
-        int j=start-1;   //4
-        for(int i=start;i<=end-1;i++){
-            if(arr[i]<povi){
-                j++;
-                swap(arr,j,i);
-
-            }
-        }
-        swap(arr,j,end);
-        return j+1;
-    }
-    private  static  void swap(int arr[],int a,int b){
+    static int partition(int arr[], int low, int high)
+    {
+       int pivot=arr[high];
+       int j=low;
+       for(int i=low;i<=high-1;i++){
+           if(arr[i]<pivot){
+             
+               swap(arr,i,j);
+                 j++;
+           }
+       }
+       swap(arr,j,high);
+       return j;
+       
+    } 
+    private static void swap(int arr[],int a,int b){
         int temp=arr[a];
         arr[a]=arr[b];
         arr[b]=temp;
     }
-
 }
